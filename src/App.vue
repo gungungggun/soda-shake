b3dca15fb8fd1438231ecb31eea7847b0bcfb579
gun <gungungggun@gmail.com>
2017-07-12 19:39:06 +0900

    slickの導入など

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
@@ -11,13 +10,13 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+<style lang="stylus">
+body
+  margin 0
+  padding 0
+  width 100%
+#app
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
