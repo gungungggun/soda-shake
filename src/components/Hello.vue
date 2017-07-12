7589adac2cf508d9734e0e4f911cb6a428eb8b86
gun <gungungggun@gmail.com>
2017-07-12 21:12:12 +0900

    ウォークスルー調整

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,12 +2,23 @@
   div
     div.walk
       slick(ref="slick" :options="slickOptions")
-        div.walk1
-          span Step1
-        div.walk2
-          span Step2
-        div.walk3
-          span Step3
+        div.inner.walk1
+          div
+            p.step STEP1
+            h1 ウォークスルー
+              br
+              | サンプル
+            p slick使って簡単に作るよ
+        div.inner.walk2
+          div
+            p.step STEP2
+            h1 スタイルシートが肝
+            p あれこれ凝ればもっといい感じになる
+        div.inner.walk3
+          div
+            p.step STEP3
+            h1 アニメーションも
+            p 時間があればアニメーションをくわえるとなお良い感じになる
 </template>
 
 <script>
@@ -22,7 +33,9 @@ export default {
     return {
       slickOptions: {
         dots: true,
-        arrows: false
+        arrows: false,
+        infinite: false,
+        edgeFriction: 0
       }
     }
   }
@@ -33,10 +46,48 @@ export default {
 .walk
   width 100%
   height 100vh
+  .inner
+    display flex
+    box-sizing border-box
+    padding 20px
+    color #fff
+    height 100vh
+    justify-content center
+    > div
+      margin-top 25vh
+      .step
+        font-size 20px
+        font-weight bold
   .walk1
-    height 80vh
-    background #f00
+    background #005cce
   .walk2
-    height 80vh
-    background #ff0
+    background #009e5d
+  .walk3
+    background #08abd0
+  .slick-dots
+    border-top 1px solid #fff
+    margin 0 auto
+    padding 10px 0 20px
+    list-style none
+    position fixed
+    bottom 0
+    display flex
+    justify-content center
+    align-items center
+    width 100%
+    li
+      width 30px
+      text-align center
+      &.slick-active
+        button
+          background #49a7c3
+      button
+        background #fff
+        border none
+        width 12px
+        height 12px
+        cursor pointer
+        border-radius 50%
+        font-size 0
+        outline none
 </style>
