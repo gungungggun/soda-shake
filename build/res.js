853fbb39a10d22f4ed54eff986f0276e2db53c1c
Gun <gungungggun@gmail.com>
2017-07-19 22:12:49 +0900

    アイコン作成

--- build/res.js
+++ build/res.js
@@ -1,5 +1,17 @@
 var sharp = require('sharp')
 
 sharp('res/icon1024x1024.png')
+  .resize(36, 36)
+  .toFile('app/platforms/android/res/mipmap-ldpi.png')
+
+sharp('res/icon1024x1024.png')
+  .resize(48, 48)
+  .toFile('app/platforms/android/res/mipmap-mdpi.png')
+
+sharp('res/icon1024x1024.png')
   .resize(72, 72)
-  .toFile('output.png')
+  .toFile('app/platforms/android/res/mipmap-hdpi.png')
+
+sharp('res/icon1024x1024.png')
+  .resize(96, 96)
+  .toFile('app/platforms/android/res/mipmap-xhdpi.png')
