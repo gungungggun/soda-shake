150617c3f929426261125a91fb114c2676cd8266
Gun <gungungggun@gmail.com>
2017-07-21 11:08:47 +0900

    build修正

--- build/res.js
+++ build/res.js
@@ -2,16 +2,16 @@ var sharp = require('sharp')
 
 sharp('res/icon1024x1024.png')
   .resize(36, 36)
-  .toFile('app/platforms/android/res/mipmap-ldpi.png')
+  .toFile('app/platforms/android/res/mipmap-ldpi/icon.png')
 
 sharp('res/icon1024x1024.png')
   .resize(48, 48)
-  .toFile('app/platforms/android/res/mipmap-mdpi.png')
+  .toFile('app/platforms/android/res/mipmap-mdpi/icon.png')
 
 sharp('res/icon1024x1024.png')
   .resize(72, 72)
-  .toFile('app/platforms/android/res/mipmap-hdpi.png')
+  .toFile('app/platforms/android/res/mipmap-hdpi/icon.png')
 
 sharp('res/icon1024x1024.png')
   .resize(96, 96)
-  .toFile('app/platforms/android/res/mipmap-xhdpi.png')
+  .toFile('app/platforms/android/res/mipmap-xhdpi/icon.png')
